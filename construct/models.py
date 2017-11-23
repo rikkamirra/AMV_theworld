8a1d0f1b7b7ce298cf3b3f86c72c04dc9442a1e2
Mariya Axamit <rikka1128@gmail.com>
2017-11-23 11:16:55 +0300

    Add picture by default

--- construct/models.py
+++ construct/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class World(models.Model):
     name = models.CharField(max_length=255)
     author = models.ForeignKey('user.Account', on_delete=models.CASCADE)
-    picture = models.URLField(max_length=255, null=True, blank=True)
+    picture = models.URLField(max_length=255, null=True, blank=True, default='http://res.cloudinary.com/drjvh4g6x/image/upload/v1506594585/browseruploads/nih2x4dfjj31jpa3tmu9.jpg')
     is_private = models.BooleanField(default=True)
 
     is_deleted = models.BooleanField(default=False)
